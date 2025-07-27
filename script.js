fde230034d35077c92a371d1f33bd9c758476812
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-07-27 09:58:23 +0100

    Examples of arrays objects etc

---
+++ script.js
@@ -0,0 +1,29 @@
+let myArray = ['Aisha', 'Irenimoyan', 'Faith', 'Sam'];
+            // 0           1            2       3
+
+
+// for (let i = 1; i < 10; i *= 2){
+//     console.log(i);
+// }
+// const myObj = {
+//     name : 'Aisha',
+//     age : 75,
+//     hobbie: "eating",
+//     color:'yellow',
+//     course: ['math', 'english', 'science'],
+//     meals: {
+//         morning: 'eggs',
+//         afternoon: 'rice',
+//         evening: 'swallow',
+//     }
+// }
+
+
+const name = 'Micheal'; //integer
+const age = 17; //string
+
+if (name === 'alex' || age === 17) {
+    console.log('it works!');
+}else {
+    console.log('it doesnt work');
+}
