d17adbff412e86b375ce8c9d1fb5128c8f6d2dfa
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-07-27 10:16:54 +0100

    Working with Variables and Data Types

---
+++ practice.js
@@ -0,0 +1,19 @@
+let name = 'Aisha';
+let age = 5;
+let isStudent = true;
+let address = {
+    city: 'Kano',
+    country: 'Nigeria'
+};
+let hobbies = ['boxing', 'surfing', 'arhery', 'cooking', 'eating'];
+let undefinedVariable;
+let nullVariable = null;
+
+console.log(name, typeof(name));
+console.log(age, typeof(age));
+console.log(isStudent, typeof(isStudent));
+console.log(address, typeof(address));
+console.log(hobbies, typeof(hobbies));
+console.log(undefinedVariable, typeof(undefinedVariable));
+console.log(nullVariable, typeof(nullVariable));
+
