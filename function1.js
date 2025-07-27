f08f658c438894c1dbfbd307132ad0db5b9cc909
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-07-27 11:20:16 +0100

    Conditional Logic Practice

---
+++ function1.js
@@ -0,0 +1,8 @@
+let number = prompt('enter a number');
+ if (number > 0){
+    alert(`${number} is positive`);
+ }else if (number < 0){
+    alert(`${number} is negative`);
+ }else{
+    alert(`${number} is zero`)
+ };
