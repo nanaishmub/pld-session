dca902a3a690c58c8b5013dc2647c39109b5662f
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-08-03 11:18:07 +0100

    filterEvenNumbers function

--- array.js
+++ array.js
@@ -2,3 +2,16 @@ let num =[1, 2, 3, 4, 5];
 num.push(6);
 num.shift();
 console.log(num);
+
+function filterEvenNumbers(...number){
+    let evenNumbers = [];
+    number.forEach(num => {
+        if (num % 2 === 0){
+            evenNumbers.push(num);
+        }
+    })
+
+    return evenNumbers;
+}
+
+console.log(filterEvenNumbers(15, 24, 78, 97, 101, 65));
