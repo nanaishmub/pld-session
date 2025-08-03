49038081d027a5450b872519e65cfe4259725c23
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-08-03 10:54:28 +0100

    Exercise 1: Destructuring Object Properties and Logging

---
+++ destructor.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "Max",
+    age: 56,
+    profession: 'Teacher'
+}
+
+const {name, age, profession} = person;
+
+console.log(`My name is ${name}. I am ${age} and I am a ${profession}`);
