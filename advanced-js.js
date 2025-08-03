d73e8f15572183ef0882cd43bc2c0fa06d3be07b
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-08-03 10:36:18 +0100

    Object and Array Destructors

---
+++ advanced-js.js
@@ -0,0 +1,15 @@
+// //Array destructor
+// const myArray = ['apple', 'mango', 'orange', 'blueberry'];
+// const [first, second, third] = myArray;
+// console.log(`My favourite fruit is a ${second}`);
+// // console.log(myArray);
+
+//Object Destructor
+const person = {
+    name: 'Sam',
+    age: 17,
+}
+
+const {name, age} = person;
+
+console.log(`My name is ${name}, I am ${age} years old`);
